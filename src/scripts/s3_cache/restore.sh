681a78ceb9b4016e0c1032b2dfc6ee0434648616
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-27 11:26:59 +0530

    docker cache layer

---
+++ src/scripts/s3_cache/restore.sh
@@ -0,0 +1,65 @@
+# !/bin/bash
+
+# inputs
+# - BUCKET_NAME
+# - KEY
+
+# Set the S3 bucket name and directory path
+S3_BUCKET_NAME="${BUCKET_NAME}"
+S3_DIRECTORY_PATH="dl_key_cache/"
+PREFIX="${KEY}"
+
+# add trailing slash if not present
+S3_DIRECTORY_PATH=${S3_DIRECTORY_PATH%/}/
+
+# Fetch all directories in the S3 directory matching the prefix
+S3_DIRECTORY_NAMES=$(aws s3api list-objects-v2 \
+    --bucket "$S3_BUCKET_NAME" \
+    --prefix "$S3_DIRECTORY_PATH$PREFIX" \
+    --delimiter / \
+    --query 'CommonPrefixes[].Prefix' \
+    --output text \
+    --region us-east-1)
+
+
+# Remove any directories that don't have a "path.txt" file
+VALID_DIRECTORIES=""
+for DIR in $S3_DIRECTORY_NAMES; do
+    OBJECT_KEY="$DIR""path.txt"
+    exists=$(aws s3 ls "s3://$S3_BUCKET_NAME/$OBJECT_KEY" --region us-east-1)
+    if [[ -n "$exists" ]]; then
+        VALID_DIRECTORIES+="$DIR"$'\n'
+    fi
+done
+
+S3_DIRECTORY_NAMES=$VALID_DIRECTORIES
+# echo $S3_DIRECTORY_NAMES
+
+
+if [[ "$S3_DIRECTORY_NAMES" = 'None' ]]; then
+    S3_DIRECTORY_NAMES=""
+fi
+
+# Extract the directory names and timestamps
+DIRECTORY_NAMES_AND_TIMESTAMPS=""
+for DIRECTORY_NAME in $S3_DIRECTORY_NAMES; do
+    DIRECTORY_NAME_WITHOUT_PREFIX=$(echo "$DIRECTORY_NAME" | sed "s|$S3_DIRECTORY_PATH||")
+    DIRECTORY_NAME_WITHOUT_PREFIX_AND_TIMESTAMP=$(echo "$DIRECTORY_NAME_WITHOUT_PREFIX" | rev | cut -d'-' -f2- | rev)
+    DIRECTORY_TIMESTAMP=$(echo "$DIRECTORY_NAME" | rev | cut -d'-' -f1 | rev)
+    DIRECTORY_NAMES_AND_TIMESTAMPS+="$DIRECTORY_NAME_WITHOUT_PREFIX_AND_TIMESTAMP $DIRECTORY_TIMESTAMP"$'\n'
+done
+
+# Find the latest directory timestamp
+LATEST_DIRECTORY_TIMESTAMP=$(echo "$DIRECTORY_NAMES_AND_TIMESTAMPS" | sort -k2 -r | head -n1 | awk '{print $2}')
+
+# Find the directory name with the latest timestamp
+LATEST_DIRECTORY_NAME=$(echo "$DIRECTORY_NAMES_AND_TIMESTAMPS" | grep "$LATEST_DIRECTORY_TIMESTAMP$" | awk '{print $1}')
+
+RESULT=$LATEST_DIRECTORY_NAME-$LATEST_DIRECTORY_TIMESTAMP
+
+# save path.txt value to a variable named RESULT_PATH
+url="s3://$S3_BUCKET_NAME/$S3_DIRECTORY_PATH$RESULT""path.txt"
+RESULT_PATH=$(aws s3 cp $url -)
+
+# download $RESULT in $RESULT_PATH
+aws s3 cp s3://$S3_BUCKET_NAME/$S3_DIRECTORY_PATH$RESULT $RESULT_PATH --recursive
---
+++ src/scripts/s3_cache/store.sh
@@ -0,0 +1,50 @@
+# !/bin/bash
+
+# inputs
+# - BUCKET_NAME
+# - KEY
+# - DIR
+
+# Set the S3 bucket name and directory path
+S3_BUCKET_NAME="${BUCKET_NAME}"
+S3_DIRECTORY_PATH="dl_key_cache/"
+
+# add trailing slash if not present
+S3_DIRECTORY_PATH=${S3_DIRECTORY_PATH%/}/
+
+# Set the prefix to search for (e.g. "key1-key2")
+KEY="${KEY}"
+PATH_="${DIR}"
+
+# remove / at the ending of PATH_ if exists
+PATH_=${PATH_%/}
+
+# check if path exists
+if [ ! -d "$PATH_" ]; then
+  echo "$PATH_ does not exist."
+  exit 0
+fi
+
+# echo current timestamp
+timestamp=$(date +%s)
+
+# remove hyphen at the ending of key if exists
+KEY=${KEY%-}
+
+# key with timestamp
+KEY_TIMESTAMP="$KEY--$timestamp"
+
+echo s3://$S3_BUCKET_NAME/$S3_DIRECTORY_PATH$KEY_TIMESTAMP
+
+# upload path directory to s3
+aws s3 cp $PATH_ s3://$S3_BUCKET_NAME/$S3_DIRECTORY_PATH$KEY_TIMESTAMP/ --recursive
+
+# upload path to s3
+echo "$PATH_" | aws s3 cp - s3://$S3_BUCKET_NAME/$S3_DIRECTORY_PATH$KEY_TIMESTAMP/path.txt
+
+
+
+
+
+
+
