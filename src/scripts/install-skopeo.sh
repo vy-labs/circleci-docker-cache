f4bf10c1a62b24f5d499449d3b757bcef97d3e7d
Ankit Kumar <ankitkr@users.noreply.github.com>
2023-04-14 21:28:26 +0530

    Update install-skopeo.sh

--- src/scripts/install-skopeo.sh
+++ src/scripts/install-skopeo.sh
@@ -6,7 +6,7 @@ VERSION=${VERSION%.*}
 CHECKPOINT_VERSION=22
 if [[ $VERSION -lt $CHECKPOINT_VERSION ]]; then
         echo "deb ${SKOPEO_LINK}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-        curl -fsSL "${SKOPEO_LINK}/Release.key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_stable.gpg > /dev/null | true
+        curl -fsSL "${SKOPEO_LINK}/Release.key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_stable.gpg > /dev/null
 fi
 
 sudo apt-get update
